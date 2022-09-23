4db3f19cc696ce2b510ef928067c6741b245248a
htnnth102 <nganhtth2105010@fpt.edu.vn>
2022-09-23 13:44:05 +0700

    meta

---
+++ meta_class.sql
@@ -0,0 +1,83 @@
+INSERT INTO meta3.meta_class (class_id,class_name,container_class_id,class_typnm) VALUES
+	 (15,'표준신청/도메인신청',4,'Object'),
+	 (16,'표준신청/기본워드신청',4,'Object'),
+	 (17,'표준신청/도메인워드신청',4,'Object'),
+	 (18,'표준신청/표준용어신청',4,'Object'),
+	 (19,'표준신청/업무용어신청',4,'Object'),
+	 (21,'표준신청/도메인데이터타입',4,'Object'),
+	 (1001,'DBMS종류',1000,'Object'),
+	 (1002,'접속구분',1000,'Object'),
+	 (1003,'이관관리변경종류',1000,'Object'),
+	 (1004,'표준화신청종류',1000,'Object');
+INSERT INTO meta3.meta_class (class_id,class_name,container_class_id,class_typnm) VALUES
+	 (1005,'표준화신청상태',1000,'Object'),
+	 (1006,'유효값전송구분',1000,'Object'),
+	 (1007,'테이블유형코드',1000,'Object'),
+	 (1008,'Cloud구분',1000,'Object'),
+	 (1011,'모델TOOL구분',1000,'Object'),
+	 (1012,'도메인유형',1000,'Object'),
+	 (1013,'도메인유형소분류',1000,'Object'),
+	 (1015,'사용자구분',1000,'Object'),
+	 (1018,'업무구분',1000,'Object'),
+	 (1019,'작업상태',1000,'Object');
+INSERT INTO meta3.meta_class (class_id,class_name,container_class_id,class_typnm) VALUES
+	 (1021,'유사어항목구분',1000,'Object'),
+	 (1023,'테이블정합성불일치내용',1000,'Object'),
+	 (1028,'시스템구분',1000,'Object'),
+	 (1030,'주제영역(테이블스페이스)',1000,'Object'),
+	 (1320,'DPI 속성',1000,'Object'),
+	 (1321,'정보보안',1000,'Object'),
+	 (1322,'DPI카테고리',1000,'Object'),
+	 (1323,'DPI 항목구분',1000,'Object'),
+	 (1088,'이관단계',1000,'Object'),
+	 (1089,'이관신청항목',1000,'Object');
+INSERT INTO meta3.meta_class (class_id,class_name,container_class_id,class_typnm) VALUES
+	 (1090,'SR관리구분',1000,'Object'),
+	 (1093,'이관요청구분',1000,'Object'),
+	 (1094,'이관참조여부',1000,'Object'),
+	 (1095,'이관처리상태',1000,'Object'),
+	 (1195,'이관결재상태',1000,'Object'),
+	 (1252,'데이터타입구분',1000,'Object'),
+	 (1253,'업무용어-데이터타입',1000,'Object'),
+	 (1256,'테이블신청사유',1000,'Object'),
+	 (1257,'연관검색조건',1000,'Object'),
+	 (1260,'표준범위유형',1000,'Object');
+INSERT INTO meta3.meta_class (class_id,class_name,container_class_id,class_typnm) VALUES
+	 (1261,'뷰컬럼 변환 규칙',1000,'Object'),
+	 (1300,'메세지코드-언어',1000,'Object'),
+	 (1301,'메세지코드-구분',1000,'Object'),
+	 (1302,'메세지코드-유형',1000,'Object'),
+	 (1303,'메세지코드-채번',1000,'Object'),
+	 (1304,'보안등급관리',1000,'Object'),
+	 (1305,'DB구문관리',1000,'Object'),
+	 (1374,'대역별구분',1000,'Object'),
+	 (1097,'코드불일치구분',1000,'Object'),
+	 (1020,'정합성체크DB구분',1000,'Object');
+INSERT INTO meta3.meta_class (class_id,class_name,container_class_id,class_typnm) VALUES
+	 (1098,'코드비교대상구분',1000,'Object'),
+	 (3001,'테이블표준불일치내용',1000,'Object'),
+	 (3002,'비표준구분',1000,'Object'),
+	 (3003,'비표준용어-데이터타입',1000,'Object'),
+	 (2127,'결재제품군',1000,'Object'),
+	 (2128,'결재가능메뉴',1000,'Object'),
+	 (1082,'스토리지버퍼POOL',1000,'Object'),
+	 (1083,'ByteUnit',1000,'Object'),
+	 (1084,'FlashCache',1000,'Object'),
+	 (1085,'파티션종류',1000,'Object');
+INSERT INTO meta3.meta_class (class_id,class_name,container_class_id,class_typnm) VALUES
+	 (1086,'파티션인덱스종류',1000,'Object'),
+	 (1070,'주기코드(보관주기,삭제주기등)',1000,'Object'),
+	 (1306,'개인정보변환대상_변환규칙',1000,'Object'),
+	 (1307,'보안정책명',1000,'Object'),
+	 (1258,'DB오브젝트유형',1000,'Object'),
+	 (1450,'연관테이블객체구분',1000,'Object'),
+	 (2100,'배치경고수신자목록',1000,'Object'),
+	 (1073,'증가주기',1000,'Object'),
+	 (1074,'삭제주기',1000,'Object'),
+	 (1075,'갱신주기',1000,'Object');
+INSERT INTO meta3.meta_class (class_id,class_name,container_class_id,class_typnm) VALUES
+	 (8001,'API_인증키',1000,'Object'),
+	 (7100,'사용자 동기화(파일) 메타항목ID',1000,'Object'),
+	 (3100,'사용자 동기화(파일) 파일인코딩',1000,'Object'),
+	 (1111,'통합검색유형',1000,'Object'),
+	 (1660,'로그이력구분',1000,'Object');
